7a70b447832af90297fe6255aebd0cd79b191394
Kate <strugovaeg@mail.ru>
2021-10-18 17:53:28 +0500

    module7-task2

--- js/main.js
+++ js/main.js
@@ -1,8 +1,10 @@
 
 import { getOffer } from './data.js';
 import { renderCard } from './map.js';
+import { deactivateForm } from './form.js';
 
 
 const offer = getOffer();
 renderCard(offer);
 
+deactivateForm();
